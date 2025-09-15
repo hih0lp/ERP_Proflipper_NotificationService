04adc39c633a5cb8eab598ff2ba9c7206d864f6c
hih0lp <mrlibobo@yandex.ru>
2025-09-15 22:33:50 +0300

    -

--- obj/Debug/net9.0/ERP_Proflipper_NotificationService.AssemblyInfo.cs
+++ obj/Debug/net9.0/ERP_Proflipper_NotificationService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP_Proflipper_NotificationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33f5f7ca99a18a792501c325f555da876227040d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a97fd79faa1b1719a6d1185a582fec59cbb7238")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP_Proflipper_NotificationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP_Proflipper_NotificationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
