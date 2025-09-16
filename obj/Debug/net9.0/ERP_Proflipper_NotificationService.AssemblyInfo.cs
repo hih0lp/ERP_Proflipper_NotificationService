03761cbf9a3b40edbb664470c92b5c0da0e84ea4
hih0lp <mrlibobo@yandex.ru>
2025-09-16 20:59:35 +0300

    add some origins in cors

--- obj/Debug/net9.0/ERP_Proflipper_NotificationService.AssemblyInfo.cs
+++ obj/Debug/net9.0/ERP_Proflipper_NotificationService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP_Proflipper_NotificationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a97fd79faa1b1719a6d1185a582fec59cbb7238")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dda2eec57f2b91aa3d118c0c866b6034a7f1c375")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP_Proflipper_NotificationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP_Proflipper_NotificationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
